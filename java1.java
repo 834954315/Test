9aa48987d73a235f9ecd0cb6df3c564205462d1b
834954315 <jizhongxiang66@126.com>
2019-07-30 23:21:47 +0800

    第一个java文件提交到仓库

---
+++ java1.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Comparator;
+
+public class test implements Comparable,Comparator{
+
+	public static void main(String[] args) {
+
+		
+		System.out.println(Integer.valueOf(1) ==Integer.valueOf(1));
+
+	}
+
+}
