0d2a5e1f13a69b4cd2831d91dad03a95b89566b4
834954315 <jizhongxiang66@126.com>
2019-09-15 15:27:46 +0800

    Update java1.java

--- java1.java
+++ java1.java
@@ -5,8 +5,7 @@ import java.util.Comparator;
 public class test implements Comparable,Comparator{
 	public void test(){
 		System.out.println("111111111111111111")
-		System.out.println("222222222222222222")
-		System.out.println("333333333333333333")	
+		 
 	
 	}
 
@@ -14,7 +13,7 @@ public class test implements Comparable,Comparator{
 
 		
 		System.out.println(Integer.valueOf(1) ==Integer.valueOf(1));
-		System.out.println("2222222222222222222222")
+		System.out.println("111111111111111111")
 
 	}
 
