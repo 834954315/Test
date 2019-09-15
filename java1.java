c94f69135c9265d27b00e5f7e3e04a37970c65a2
834954315 <jizhongxiang66@126.com>
2019-09-15 14:40:55 +0800

    11111111

--- java1.java
+++ java1.java
@@ -5,6 +5,8 @@ import java.util.Comparator;
 public class test implements Comparable,Comparator{
 	public void test(){
 		System.out.println("111111111111111111")
+		System.out.println("222222222222222222")
+	
 	}
 
 	public static void main(String[] args) {
