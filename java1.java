1adc60ddda47424ef921160fe8fd6d9be74e4a36
834954315 <jizhongxiang66@126.com>
2019-09-15 14:36:23 +0800

    Update java1.java

--- java1.java
+++ java1.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class test implements Comparable,Comparator{
 	public void test(){
-		System.out.println("111111111111111111")
+		System.out.println("33333333333333333")
 	}
 
 	public static void main(String[] args) {
